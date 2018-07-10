44d5050915f1b332bf937471aea5accdd050afad
Lexie <oleksandrapas@gmail.com>
2018-07-10 15:21:44 +0200

    Filter locations functionality is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleMap from './GoogleMap.js';
 import List from './List.js';
 import './App.css';
+import escapeRegExp from 'escape-string-regexp';
 
 class App extends Component {
   state = {
@@ -11,15 +12,33 @@ class App extends Component {
         {key: 'park-moczydlo', title: 'Park Moczydlo', location: {lat: 52.2409607, lng: 20.9532269}},
         {key: 'warsaw-uprising-museum', title: 'Warsaw Uprising Museum', location: {lat: 52.232324, lng: 20.981154}},
         {key: 'arkadia', title: 'Arkadia', location: {lat: 52.2574323, lng: 20.9848839}}
-        ]
+        ],
+        query: '',
+        filteredLocations: []
+  }
+
+filterLocations = (query) => {
+  this.setState({query: query})
   }
 
   render() {
+     let filteredLocations
+        if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query),'i')
+     filteredLocations = this.state.locations.filter((location)=> match.test(location.title))
+  } else {
+    filteredLocations = this.state.locations
+    }
 
     return (
       <div className="app">
-       <List locations={this.state.locations}/>
-       <GoogleMap locations={this.state.locations} />
+       <List 
+        locations={this.state.locations}
+        filteredLocations={filteredLocations}
+        query={this.state.query}
+        filterLocations={this.filterLocations}
+        />
+       <GoogleMap locations={filteredLocations} />
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class List extends Component {
-  
+ 
+  updateQuery =(query) => {
+    this.props.filterLocations(query)
+  } 
+
   render() {
     const { locations } = this.props;
 
@@ -9,11 +13,14 @@ class List extends Component {
       <aside className="list">
        <h2 className="title">Popular Places</h2>
        <div className="filter">
-       <input className="filterInput" placeholder="Filter locations"/>
+       <input
+       value={this.props.query}
+       onChange={(event) => this.updateQuery(event.target.value)} 
+       className="filterInput" placeholder="Filter locations"/>
        <button className="filterBtn">Filter</button>
        </div>
         <ul>
-        {locations.map(location =>
+        {this.props.filteredLocations.map((location) =>
           <li key={location.key}>{location.title}</li>
           )}
         </ul>
